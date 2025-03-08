6e43827ca4236ff3ddc33fc52bf892e46d462fb3
ykyki <bsyki@pm.me>
2025-03-08 14:41:54 +0900

    HelloTest is fixed

--- dom-func/src/test/kotlin/practice/kotlin/dom/func/HelloTest.kt
+++ dom-func/src/test/kotlin/practice/kotlin/dom/func/HelloTest.kt
@@ -3,7 +3,12 @@ package practice.kotlin.dom.func
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
+import io.mockk.Runs
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 
@@ -14,18 +19,42 @@ class HelloTest : FunSpec({
         }
     }
 
-    test("sayHello calls printer n times") {
-        checkAll(Arb.int(0..10)) { n ->
+    test("sayHello calls printer n times when n is non-negative") {
+        val printer = mockk<(String) -> Unit>()
+
+        checkAll(
+            20, // reduce the number of iterations to avoid slow test
+            Arb.int(0, 1000), // too large n may cause a stack overflow because of mockk
+            Arb.string(),
+        ) { n, s ->
             // given
-            val name = "Kotlin"
-            val printerMock = mockk<(String) -> Unit>(relaxed = true)
-            println("n = $n")
+            every { printer.invoke(any()) } just Runs
 
             // when
-            Hello.sayHello(name, n, printerMock)
+            println("n = $n, s = $s")
+            Hello.sayHello(s, n, printer)
 
             // then
-            verify(exactly = n) { printerMock("Hello, $name!\n") }
+            verify(exactly = n) { printer("Hello, $s!\n") }
+            verify(exactly = n) { printer(any()) }
+
+            // cleanup
+            clearMocks(printer)
+        }
+    }
+
+    test("sayHello does not call printer when n is negative") {
+        checkAll(Arb.int(Int.MIN_VALUE, -1), Arb.string()) { n, s ->
+            // given
+            val printer = mockk<(String) -> Unit>()
+            every { printer.invoke(any()) } just Runs
+
+            // when
+            println("n = $n, s = $s")
+            Hello.sayHello(s, n, printer)
+
+            // then
+            verify(exactly = 0) { printer(any()) }
         }
     }
 })
