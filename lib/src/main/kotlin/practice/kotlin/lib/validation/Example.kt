7ece34fb0dff9c921097020750086da3954ff284
ykyki <bsyki@pm.me>
2025-03-23 03:28:13 +0900

    Validation is created

---
+++ lib/src/main/kotlin/practice/kotlin/lib/validation/Example.kt
@@ -0,0 +1,123 @@
+@file:Suppress("MatchingDeclarationName", "MagicNumber")
+
+package practice.kotlin.lib.validation
+
+import java.time.Instant
+import java.time.format.DateTimeParseException
+
+@JvmInline
+internal value class FormatError(val value: String)
+
+internal fun main() {
+    // val user = User.tryFrom("Alice", 12, "2021-01-01T00:00:00Z")
+    val user = User.tryFrom("Ali ce", -12, "2021-01-01T00:00:00T+09:00a")
+    println(user)
+}
+
+internal data class User(
+    val name: UserName,
+    val age: UserAge,
+    val updatedAt: UserUpdatedAt,
+) {
+    companion object {
+        fun tryFrom(
+            name: String,
+            age: Int,
+            updatedAt: String,
+        ): Validation<User, FormatError> =
+            Validation.map3(
+                UserName.tryFrom(name),
+                UserAge.tryFrom(age),
+                UserUpdatedAt.tryFrom(updatedAt),
+            ) { name, age, updatedAt -> User(name, age, updatedAt) }
+    }
+}
+
+internal data class UserName(val valur: String) {
+    internal companion object {
+        private const val NAME = "username"
+
+        fun tryFrom(s: String): Validation<UserName, FormatError> =
+            Validation.map3(
+                s.shouldBeLongerThan(3, NAME),
+                s.shouldBeShorterThan(20, NAME),
+                s.shouldNotContainWhitespace(NAME),
+            ) { _, _, _ -> UserName(s) }
+    }
+}
+
+internal data class UserAge(val value: Int) {
+    internal companion object {
+        private const val NAME = "age"
+
+        fun tryFrom(n: Int): Validation<UserAge, FormatError> =
+            Validation.map2(
+                n.shouldBeGreaterThan(0, NAME),
+                n.shouldBeLessThan(150, NAME),
+            ) { _, _ -> UserAge(n) }
+    }
+}
+
+internal data class UserUpdatedAt(val value: Instant) {
+    internal companion object {
+        private const val NAME = "updated_at"
+
+        fun tryFrom(s: String): Validation<UserUpdatedAt, FormatError> =
+            s.shouldBeValidTimestamp(NAME).map { UserUpdatedAt(it) }
+    }
+}
+
+internal fun String.shouldNotBeEmpty(
+    name: String = "input"
+): Validation<String, FormatError> = when {
+    isNotEmpty() -> Validation.ok(this)
+    else -> Validation.err(FormatError("$name should not be empty, but it is"))
+}
+
+internal fun String.shouldBeLongerThan(
+    n: Int,
+    name: String = "input"
+): Validation<String, FormatError> = when {
+    length > n -> Validation.ok(this)
+    else -> Validation.err(FormatError("$name should be longer than $n, but its length is $length"))
+}
+
+internal fun String.shouldBeShorterThan(
+    n: Int,
+    name: String = "input"
+): Validation<String, FormatError> = when {
+    length < n -> Validation.ok(this)
+    else -> Validation.err(FormatError("$name should be shorter than $n, but its length is $length"))
+}
+
+internal fun String.shouldNotContainWhitespace(
+    name: String = "input"
+): Validation<String, FormatError> = when {
+    contains(Regex("\\s")) -> Validation.err(FormatError("$name should not contain whitespace, but it does"))
+    else -> Validation.ok(this)
+}
+
+internal fun String.shouldBeValidTimestamp(
+    name: String = "input"
+): Validation<Instant, FormatError> =
+    try {
+        Validation.ok(Instant.parse(this))
+    } catch (e: DateTimeParseException) {
+        Validation.err(FormatError("$name should be a valid timestamp, but it is not"))
+    }
+
+internal fun Int.shouldBeLessThan(
+    n: Int,
+    name: String = "input"
+): Validation<Int, FormatError> = when {
+    this < n -> Validation.ok(this)
+    else -> Validation.err(FormatError("$name should be less than $n, but it is not"))
+}
+
+internal fun Int.shouldBeGreaterThan(
+    n: Int,
+    name: String = "input"
+): Validation<Int, FormatError> = when {
+    this > n -> Validation.ok(this)
+    else -> Validation.err(FormatError("$name should be greater than $n, but it is not"))
+}
---
+++ lib/src/main/kotlin/practice/kotlin/lib/validation/Validation.kt
@@ -0,0 +1,113 @@
+package practice.kotlin.lib.validation
+
+import com.github.michaelbull.result.Result
+
+public sealed class Validation<out A, out E : Any> {
+    public companion object {
+        public fun <A> ok(a: A): Validation<A, Nothing> = Ok(a)
+        public fun <E : Any> err(e: E): Validation<Nothing, E> = Err(e)
+
+        public fun <A, E : Any> unit(a: A): Validation<A, E> = ok(a)
+
+        public infix fun <A, B, E : Any> (Validation<(A) -> B, E>).apply(
+            fa: Validation<A, E>,
+        ): Validation<B, E> = when (this) {
+            is Ok -> when (fa) {
+                is Ok -> ok(this.value(fa.value))
+                is Err -> fa
+            }
+
+            is Err -> when (fa) {
+                is Ok -> this
+                is Err -> Err(this.head, this.tail + fa.errors())
+            }
+        }
+
+        public fun <A, B, E : Any> map(
+            fa: Validation<A, E>,
+            f: (A) -> B
+        ): Validation<B, E> = when (fa) {
+            is Ok -> ok(f(fa.value))
+            is Err -> fa
+        }
+
+        public fun <A, B, C, E : Any> map2(
+            fa: Validation<A, E>,
+            fb: Validation<B, E>,
+            f: (A, B) -> C
+        ): Validation<C, E> =
+            unit<(A) -> (B) -> C, E> { a -> { b -> f(a, b) } }
+                .apply(fa)
+                .apply(fb)
+
+        public fun <A, B, C, D, E : Any> map3(
+            fa: Validation<A, E>,
+            fb: Validation<B, E>,
+            fc: Validation<C, E>,
+            f: (A, B, C) -> D
+        ): Validation<D, E> =
+            unit<(A) -> (B) -> (C) -> D, E> { a -> { b -> { c -> f(a, b, c) } } }
+                .apply(fa)
+                .apply(fb)
+                .apply(fc)
+
+        public fun <A, B, C, D, E, Err : Any> map4(
+            fa: Validation<A, Err>,
+            fb: Validation<B, Err>,
+            fc: Validation<C, Err>,
+            fd: Validation<D, Err>,
+            f: (A, B, C, D) -> E
+        ): Validation<E, Err> =
+            unit<(A) -> (B) -> (C) -> (D) -> E, Err> { a -> { b -> { c -> { d -> f(a, b, c, d) } } } }
+                .apply(fa)
+                .apply(fb)
+                .apply(fc)
+                .apply(fd)
+
+        @Suppress("LongParameterList")
+        public fun <A, B, C, D, E, F, Err : Any> map5(
+            fa: Validation<A, Err>,
+            fb: Validation<B, Err>,
+            fc: Validation<C, Err>,
+            fd: Validation<D, Err>,
+            fe: Validation<E, Err>,
+            f: (A, B, C, D, E) -> F
+        ): Validation<F, Err> =
+            @Suppress("ParameterListWrapping", "Wrapping", "MaximumLineLength", "MaxLineLength", "ArgumentListWrapping")
+            unit<(A) -> (B) -> (C) -> (D) -> (E) -> F, Err> { a -> { b -> { c -> { d -> { e -> f(a, b, c, d, e) } } } } } // @formatter:off
+                .apply(fa)
+                .apply(fb)
+                .apply(fc)
+                .apply(fd)
+                .apply(fe)
+
+        public fun <A, B, E : Any> traverse(
+            la: List<A>,
+            f: (A) -> Validation<B, E>,
+        ): Validation<List<B>, E> =
+            la.fold(unit(mutableListOf<B>())) { acc, a ->
+                map2(acc, f(a)) { bs, b ->
+                    bs.apply { add(b) }
+                }
+            }
+    }
+
+    public fun isOk(): Boolean = this is Ok
+    public fun isErr(): Boolean = this is Err
+
+    public fun <B> map(f: (A) -> B): Validation<B, E> = map(this, f)
+}
+
+public fun <A, E : Any> Result<A, E>.toValidation() = when {
+    isOk -> Validation.ok(value)
+    else -> Validation.err(error)
+}
+
+internal data class Err<E : Any>(
+    val head: E,
+    val tail: List<E> = emptyList(),
+) : Validation<Nothing, E>() {
+    fun errors() = listOf(head) + tail
+}
+
+internal data class Ok<A>(val value: A) : Validation<A, Nothing>()
